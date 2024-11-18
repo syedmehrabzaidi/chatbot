75fb29008832a0564e5c28c86eb62643c58a7076
Syed Mehrab <45766880+syedmehrabzaidi@users.noreply.github.com>
2024-11-19 02:45:26 +0500

    Update main.py chnages in signup and resolved error

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ models.Base.metadata.create_all(bind=database.engine)
 
 @app.post("/signup")
 def signup(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email == user.email).first()
-    if db_user:
+    db_user_email = db.query(models.User).filter(models.User.email == user.email).first()
+    if db_user_email:
         raise HTTPException(status_code=400, detail="Email already registered")
-    
+
+    # Check if the username already exists
+    db_user_username = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user_username:
+        raise HTTPException(status_code=400, detail="Username already registered")
     hashed_password = hash_password(user.password)
     new_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+    return { "message":"User is created successfully",
+            "status" : 200,
+        "user_details": new_user}
+
 
 @app.post("/login")
 def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
